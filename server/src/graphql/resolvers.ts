097354b3ecec5a9385f506501afd72481a5535f5
uanoob <usnet@ukr.net>
2020-03-04 16:04:01 +0200

    Connect resolvers to cloud database

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,18 +1,34 @@
 import { IResolvers } from 'apollo-server-express';
-import { ListingType, listings } from '../listings';
+import { ObjectId } from 'mongodb';
+import { Database, Listing } from '../lib/types';
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: (): ListingType[] => listings,
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database },
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray();
+    },
   },
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }): ListingType => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database },
+    ): Promise<Listing> => {
+      const deleteResult = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id),
+      });
+      if (!deleteResult.value) {
+        throw new Error('failed to delete listing');
       }
-      throw new Error('failed to delete listing');
+      return deleteResult.value;
     },
+  },
+  Listing: {
+    id: (listing: Listing): string => listing._id.toString(),
+    image: (listing: Listing) => listing.image,
   },
 };
