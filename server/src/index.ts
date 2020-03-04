452e1886380e19715dc78b725916deef3a213ecf
uanoob <usnet@ukr.net>
2020-03-04 14:38:43 +0200

    Added ts types, connect app to mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,22 @@
-import express from 'express';
+require('dotenv').config();
+
+import express, { Application } from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import { connectDatabase } from './database';
 import { typeDefs, resolvers } from './graphql';
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }),
+  });
+  server.applyMiddleware({ app, path: '/api' });
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server.applyMiddleware({ app, path: '/api' });
+  app.listen(process.env.PORT);
 
-app.listen(PORT);
+  console.log(`[app]: http://localhost:${process.env.PORT}`);
+};
 
-console.log(`[app]: http://localhost:${PORT}`);
+mount(express());
